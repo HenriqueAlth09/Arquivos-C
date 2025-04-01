54a572752e5ef9c12c225e95a9a2c8a77173a47d
Henrique Althman <henriquealthman23@gmail.com>
2025-04-01 13:36:20 -0300

    Add files via upload

---
+++ Questão 7.cpp
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main( {
+    int num1, num2, soma
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num1);
+    
+   printf("Digite o segudo numero: ");
+   scanf("%f", &num2);
+   
+   num1+num2
+   printf("A soma de %d e %c eh: %d\n", num1, num2, soma);
+   
+   return 0;}
